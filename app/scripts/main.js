d274315beea8494a83d5cb24b4356f0027d87586
Jorge Lopez (iMac) <jorge@developez.org>
2015-02-24 22:33:30 +0100

    validacion codigo postal + empresa

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,75 @@
 $(document).ready(function () {
 
-    // $(function () {
-    //     $("#password").complexify({}, function (valid, complexity) {
-    //         document.getElementById("PassValue").value = complexity;
-    //     });
-    // });
+    console
 
-$('#complexify #password').complexify({}, function (valid, complexity) {
-    var progressBar = $('#complexify #complexity-bar');
+    $('#complexify #password').complexify({}, function (valid, complexity) {
+        var progressBar = $('#complexify #complexity-bar');
 
-    progressBar.toggleClass('progress-bar-success', valid);
-    progressBar.toggleClass('progress-bar-danger', !valid);
-    progressBar.css({'width': complexity + '%'});
+        progressBar.toggleClass('progress-bar-success', valid);
+        progressBar.toggleClass('progress-bar-danger', !valid);
+        progressBar.css({'width': complexity + '%'});
 
-    $('#complexify #complexity').text(Math.round(complexity) + '%');
-});
+        $('#complexify #complexity').text(Math.round(complexity) + '%');
+    });
 
-$("#miform").submit(function(event) {
-    event.preventDefault();
-    console.log("hola");
+    $("#apellidos").keyup(function () {
+        //$("#nombre_empresa").text($("#apellidos").text() + $("#apellidos").text());
+        $("#nombre_empresa").val("");
+        $("#nombre_empresa").val($("#nombre").val() + " " + $("#apellidos").val());
+    });
 
-    $("#miform").validate({
-        errorElement: 'span',
-        errorElementClass: 'has-error has-feedback',
-        errorClass: 'has-error has-feedback',
+    /* COMPROBACIONES RADIO BUTTON DEMANDANTE */
+    $("#demandante-empresa").click(function() {
+        if($("#demandante-empresa").is(':checked')) {
+            $("#empresa").text("Empresa");
+            $("#nombre_empresa").prop( "disabled", false );
+            $("#nombre_empresa").prop( "placeholder", "Empresa");
+            $("#nif-label").text("CIF:");
+            $("#nif").prop( "placeholder", "Cif");
+        }
+    });
+
+    $("#demandante-particular").click(function() {
+        if($("#demandante-particular").is(':checked')) {
+            $("#empresa").text("Nombre:");
+            $("#nombre_empresa").val($("#nombre").val() + " " + $("#apellidos").val());
+            $("#nombre_empresa").prop( "disabled", true );
+            $("#nif-label").text("NIF:");
+            $("#nif").prop( "placeholder", "Nif");
+        }
+    });
+    /* FIN COMPROBACIONES RADIO BUTTON DEMANDANTE */
+
+    /* COMPROBACIONES CODIGO POSTAL  */
+
+    $("#cp").change(function(event) {
+        if($("#cp").val().length > 5 ) {
+            $("#cp").val("");
+        }
+        if($("#cp").val().length == 4 ) {
+            $("#cp").val("0" + $("#cp").val());
+        }
+        if($("#cp").val().length == 3 ) {
+            $("#cp").val("00" + $("#cp").val());
+        }
+        if($("#cp").val().length == 2 ) {
+            $("#cp").val("000" + $("#cp").val());
+        }
+        if($("#cp").val().length == 1 ) {
+            $("#cp").val("0000" + $("#cp").val());
+        }
+    });
+
+    /* FIN COMPROBACIONES CODIGO POSTAL */
+
+    /* COMPROBACIONES FORM */
+    $("#miform").submit(function(event) {
+        event.preventDefault();
+
+        $("#miform").validate({
+            errorElement: 'span',
+            errorElementClass: 'has-error has-feedback',
+            errorClass: 'has-error has-feedback',
                 // REGLAS
                 rules: {
                     nombre: "required",
@@ -41,10 +87,13 @@ $("#miform").submit(function(event) {
                         required: true,
                         email: true
                     },
-                    cif: "required",
+                    nif: "required",
                     nombre_empresa: "required",
                     direccion: "required",
-                    cp: "required",
+                    cp: {
+                        required: true,
+                        maxlength: 5
+                    },
                     localidad: "required",
                     provincia: "required",
                     pais: "required",
@@ -64,7 +113,10 @@ $("#miform").submit(function(event) {
                 messages: {
                     nombre: "El campo nombre es requerido",
                     apellidos: "El campo apellidos es requerido",
-                    telefono: "El campo telefono es requerido y debe contener 9 digitos",
+                    telefono: {
+                        required: "El telefono es necesario",
+                        minlength: "El numero debe tener 9 caracteres como minimo"
+                    },
                     email: {
                         required: "El campo email es requerido",
                         email: "Debe ser tipo email"
@@ -97,4 +149,5 @@ $("#miform").submit(function(event) {
                 }
             });
 });
+/* FIN COMPROBACIONES RADIO BUTTON DEMANDANTE */
 });
